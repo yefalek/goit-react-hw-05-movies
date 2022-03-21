3deb518bbe1f1f0d91906b33fb1254d5f8a63357
Aleksandr Yefremenkov <yefremenkov.alek@gmail.com>
2022-03-21 12:19:34 +0200

    add ViewItem

---
+++ src/views/ViewItem.js
@@ -0,0 +1,35 @@
+import { useLocation, NavLink } from "react-router-dom";
+import PropTypes from 'prop-types';
+import slugify from 'slugify';
+import noImages from '';
+
+export default function ViewItem({ id, title, images, rating }) {
+    const location = useLocation();
+    const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+
+    const makeSlug = string =>
+        slugify(string, { lower: true, });
+    
+    return (
+        <li key={id} className="movies_item">
+            <NavLink to={{
+                pathname: `/movies/${makeSlug(`${title}${id}`)}`,
+                state: { from: location },
+            }}>
+                <div className="movies__images_container"></div>
+                <img className="movies_images"
+                    src={images ? `${IMG_URL}${images}` : noImages}
+                    alt={title}
+                    width="300"
+                />
+                <p className="movies__title">{title}</p>
+                <p className="movies__item-rating">{rating}</p>
+            </NavLink>
+        </li>
+    );
+}
+
+ViewItem.propTypes = {
+    id: PropTypes.number,
+    title: PropTypes.string,
+};
