8ababbe6042edc228b57b9579c8cf7dabd98f5c9
Aleksandr Yefremenkov <yefremenkov.alek@gmail.com>
2022-03-21 13:12:12 +0200

    add App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-export const App = () => {
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
+import Container from '';
+import AppBar from '';
+import Loader from '';
+import Footer from '';
+
+const HomeView = lazy(() => import('./views/HomeView' /* webpackChunkName: "HomeView" */),);
+const MoviesView = lazy(() => import('./' /*  webpackChunkName: "MoviesView" */),);
+const MovieDetailsPage = lazy(() => import('' /* webpackChunkName: "MovieDetailsPage" */),);
+const NotFoundView = lazy(() => import('./views/NotFoundView' /* webpackChunkName: "NotFoundView" */),);
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      Site in progress...
-    </div>
+    <>
+      <Container>
+        <AppBar/>
+          <Suspense fallback={<Loader/>}>
+            <Switch>
+              <Route path='/' exact><HomeView /></Route>
+              <Route path='/movies' exact><MoviesView /></Route>
+              <Route path='/movies/:slug'><MovieDetailsPage /></Route>
+              <Route><NotFoundView/></Route>
+            </Switch>
+          </Suspense>
+        <ToastContainer autoClose={2000}/>
+      </Container>
+      <Footer/>
+    </>
   );
-};
+}
